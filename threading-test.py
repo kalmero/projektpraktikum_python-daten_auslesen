8d37f7437833a3c3bfda889bdcd0db3d62beee32
Max <maxiwelsch@aol.com>
2013-06-26 22:04:40 +0200

    Updated threading-test so it gives text every 3s

--- threading-test.py
+++ threading-test.py
@@ -1,8 +1,10 @@
 import threading
-#Prints "who are you after 3 seconds"
+#Prints "who are you" every 3 seconds
 
 def test():
 	print "who are you?!"
+	timer = threading.Timer(3.0,test)
+	timer.start()
 
 timer = threading.Timer(3.0,test)
 timer.start()
